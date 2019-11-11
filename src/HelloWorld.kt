a97461e9b732c89be7cbc92ac1032af8893c6661
ujjwal <ujjwalmaity98@gmail.com>
2019-11-11 18:22:49 +0530

    Operator

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -10,4 +10,14 @@ fun main(args: Array<String>) {
     nameInJava.firstName = "RAHUL"
     nameInJava.lastName = "MAITY"
     println("${nameInJava.firstName} ${nameInJava.lastName}")
+
+    val num1: Int = 10
+    val num2 = 2
+    val sum = num1 + num2
+    println(sum)
+    val sub = num1 - num2
+    println("$sub")
+    println(num1 * num2)
+    val div = num1 / num2
+    println("$num1 / $num2 = $div")
 }
